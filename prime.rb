e7cc680a7adf3fa199f57f25185530f0f6f7ffe6
Jorge Naranjo <j.naranjojr@gmail.com>
2016-08-14 16:21:03 -0400

    Two tests passing

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
 class PrimeFactors
-  def generate
+  def generate(number)
+    factors = []
+    if number > 1
+      factors << 2
+    end
+    factors
   end
 end
--- prime_spec.rb
+++ prime_spec.rb
@@ -2,8 +2,15 @@ require "rspec"
 require "./prime"
 
 describe PrimeFactors do
+
   it 'generates no prime factors for input 1' do
     prime_factors = PrimeFactors.new
     expect(prime_factors.generate(1)).to eq([])
   end
+
+  it 'generates prime factors for input 2' do
+    prime_factors = PrimeFactors.new
+    expect(prime_factors.generate(2)).to eq([2])
+  end
+
 end
